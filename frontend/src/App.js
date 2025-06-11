a0ef328d7920caa05e089cf2919ff1f10d0b4383
harini1061 <harinihere11@gmail.com>
2025-06-11 06:14:22 -0700

    7th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Mood from './pages/Mood';
+import Home from './pages/Home';
 import Habit from './pages/Habit';
+import Mood from './pages/Mood';
 import Goal from './pages/Goal';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Welcome to the Habit, Mood & Goal Tracker</h1>} />
-        <Route path="/mood" element={<Mood />} />
-        <Route path="/habit" element={<Habit />} />
-        <Route path="/goal" element={<Goal />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/habits" element={<Habit />} />
+        <Route path="/moods" element={<Mood />} />
+        <Route path="/goals" element={<Goal />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App; // ✅ VERY IMPORTANT
